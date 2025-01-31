a24e221036faed6da38293c7cb5ad394536d90ab
Nayeem Bhuiyan <111002293+Nayeem746@users.noreply.github.com>
2025-02-01 02:01:15 +0600

    Update admindb.php

--- admin/model/admindb.php
+++ admin/model/admindb.php
@@ -71,6 +71,8 @@ class mydb {
         return $connobject->query($sql);
     }
 
+    //Seller
+
     public function showAllseller($table, $connobject) {
         $sql = "SELECT * FROM $table";
         $result = $connobject->query($sql);
@@ -89,7 +91,199 @@ class mydb {
         $stmt->bind_param("s", $username);
         return $stmt->execute();
     }
+    function getSellerProducts($sellerUsername, $conn) {
+        $sql = $conn->prepare("SELECT * FROM products WHERE SellerUsername = ?");
+        $sql->bind_param("s", $sellerUsername);
+        $sql->execute();
+        return $sql->get_result();
+    }
+
+    public function updateSeller($table, $SellerUsername, $Email, $Password, $BusinessName, $ProductType, $Fullname, $Phone, $Address, $District, $NID, $conn) {
+        $sql = "UPDATE $table SET 
+                    Email = '$Email',  
+                    Password = '$Password',                                       
+                    BusinessName = '$BusinessName', 
+                    ProductType = '$ProductType', 
+                    Fullname = '$Fullname', 
+                    Phone = '$Phone', 
+                    Address = '$Address', 
+                    District = '$District', 
+                    NID = '$NID' 
+                WHERE SellerUsername = '$SellerUsername'";
     
+        if ($conn->query($sql) === TRUE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    
+
+
+    //Delibvey Man
+    public function searchDeliveryman($table, $username, $conn) {
+        $sql = "SELECT * FROM $table WHERE DeliveryUsername = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        return $stmt->get_result();
+    }
+
+    public function showAllDeliveryPersons($table, $conn) {
+        $sql = "SELECT * FROM $table";
+        return $conn->query($sql);
+    }
+
+    public function updateDeliveryPerson($table, $data, $username, $conn) {
+        $sql = "UPDATE $table SET Email = ?, Password = ?, Fullname = ?, Phone = ?, Vehicle = ?, CV = ?, Age = ? WHERE DeliveryUsername = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssssssis", $data['Email'], $data['Password'], $data['Fullname'], $data['Phone'], $data['Vehicle'], $data['CV'], $data['Age'], $username);
+        return $stmt->execute();
+    }
+
+    function deleteDeliveryman($table, $DeliveryUsername, $conn) {
+        $sql = $conn->prepare("DELETE FROM $table WHERE DeliveryUsername = ?");
+        $sql->bind_param("s", $DeliveryUsername);
+        return $sql->execute();
+    }
+
+    public function updateDeliveryMan($table, $DeliveryUsername, $Email, $Password, $Fullname, $Phone, $Vehicle,$CV, $Age, $conn) {
+        // Update query for the delivery man
+        $sql = "UPDATE $table SET 
+                    Email = '$Email',
+                    Password = '$Password', 
+                    Fullname = '$Fullname', 
+                    Phone = '$Phone', 
+                    Vehicle = '$Vehicle',                                    
+                    CV = '$CV',
+                    Age = '$Age'               
+
+                WHERE DeliveryUsername = '$DeliveryUsername'";
+    
+        // Execute the query and return the result
+        if ($conn->query($sql) === TRUE) {
+            return true; // Return true if update is successful
+        } else {
+            return false; // Return false if there is an error
+        }
+    }
+    
+
+//buyer
+
+public function showAllBuyer($table, $conn) {
+    // Query to fetch all buyers from the 'buyer' table
+    $sql = "SELECT * FROM $table";
+    
+    // Prepare and execute the query
+    $stmt = $conn->prepare($sql);
+    if ($stmt->execute()) {
+        // Return the result set
+        return $stmt->get_result();
+    } else {
+        return null; // If query fails, return null
+    }
+}
+function deleteBuyer($table, $buyerUsername, $conn) {
+    $sql = $conn->prepare("DELETE FROM $table WHERE BuyerUsername = ?");
+    $sql->bind_param("s", $buyerUsername);
+    return $sql->execute();
+}
+
+public function searchBuyer($table, $username, $conn) {
+    // Query to search for a buyer by their username
+    $sql = "SELECT * FROM $table WHERE BuyerUsername = ?";
+    
+    // Prepare the query
+    $stmt = $conn->prepare($sql);
+    
+    // Bind the parameter (buyer username)
+    $stmt->bind_param("s", $username);
+    
+    // Execute the query
+    if ($stmt->execute()) {
+        // Return the result set
+        return $stmt->get_result();
+    } else {
+        return null; // If query fails, return null
+    }
+}
+
+public function updateBuyer($table, $BuyerUsername, $Email, $Password, $Fullname, $Phone, $DateOfBirth, $conn) {
+    // Update query for the buyer
+    $sql = "UPDATE $table SET 
+                Email = '$Email', 
+                Password = '$Password', 
+                Fullname = '$Fullname', 
+                Phone = '$Phone', 
+                $DateOfBirth = '$DateOfBirth'             
+            WHERE $BuyerUsername = '$BuyerUsername'";
+    
+    // Execute the query and check if successful
+    if ($conn->query($sql) === TRUE) {
+        return true; // Return true if update is successful
+    } else {
+        return false; // Return false if there is an error
+    }
+}
+
+
+
+//Employee
+
+public function showAllEmployee($table, $conn) {
+    $sql = "SELECT * FROM $table"; // Retrieve all employees
+    $stmt = $conn->prepare($sql);
+    if ($stmt->execute()) {
+        return $stmt->get_result();
+    } else {
+        return null; // If query fails, return null
+    }
+}
+
+public function searchEmployee($table, $username, $conn) {
+    $sql = "SELECT * FROM $table WHERE EmployeeUsername = ?"; // Search for employee by username
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
+    if ($stmt->execute()) {
+        return $stmt->get_result();
+    } else {
+        return null; // If query fails, return null
+    }
+}
+function deleteEmployee($table, $employeeUsername, $conn) {
+    $sql = $conn->prepare("DELETE FROM $table WHERE EmployeeUsername = ?");
+    $sql->bind_param("s", $employeeUsername);
+    return $sql->execute();
+}
+
+// Update employee details
+public function updateEmployee($table, $empUsername, $empEmail,$empPassword, $empFullName, $empPhone, $empWorkShift,  $cvPath,$empAge, $conn) {
+    // Update query for the employee
+    $sql = "UPDATE $table SET 
+                Password = '$empPassword',                
+                Email = '$empEmail',                 
+                Fullname = '$empFullName', 
+                Phone = '$empPhone', 
+                Workshift = '$empWorkShift', 
+                CV = '$cvPath',
+                Age = '$empAge'
+            WHERE EmployeeUsername = '$empUsername'";
+
+    // Execute the query and return the result
+    if ($conn->query($sql) === TRUE) {
+        return true; // Return true if update is successful
+    } else {
+        return false; // Return false if there is an error
+    }
+}
+
+
+
+
+
+
     
 }
 
